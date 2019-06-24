14a99d48c59c375319e3659818ebee7c63fc497d
Dominik Chrastecky <dominik@chrastecky.cz>
2019-06-24 15:36:53 +0200

    fix priority of compiler pass

--- src/RvltDigitalStaticDiBundle.php
+++ src/RvltDigitalStaticDiBundle.php
@@ -3,6 +3,7 @@
 namespace RvltDigital\StaticDiBundle;
 
 use RvltDigital\StaticDiBundle\DependencyInjection\Compiler\MakeServicesPublicCompilerPass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -16,6 +17,10 @@ class RvltDigitalStaticDiBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
-        $container->addCompilerPass(new MakeServicesPublicCompilerPass());
+        $container->addCompilerPass(
+            new MakeServicesPublicCompilerPass(),
+            PassConfig::TYPE_BEFORE_REMOVING,
+            -1000
+        );
     }
 }
